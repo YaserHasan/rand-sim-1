7db777cfe30df2e3400d1e7f89c1dc9730d5d425
Yaser Hasan <sweeter.yaser@gmail.com>
2022-05-15 10:35:12 +0300

    clean up

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 // Node Modules Imports
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require('express');
+const path = require('path');
 
 // Internal Modules Imports
-const api = require('./server/routes/api')
+const api = require('./server/routes/api');
 
-// Connecting to Mongo Database
-// mongoose.connect('mongodb://localhost/todos', { useNewUrlParser: true })
 
-// Setting up express, serving client files, configuring bodyParser
-const app = express()
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+// Setting up express, serving client files
+const app = express();
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.json());
 
 // Connecting to "api", i.e our routes
-app.use('/', api)
+app.use('/', api);
 
 // Running the server
-const port = 3001
-app.listen(port, function () { console.log('Running on ' + port) })
+const PORT = 5000;
+app.listen(PORT, function() {
+    console.log('Running on ' + PORT);
+});
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,33 +1,35 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
 
-const todos = []
-let id = 1
+
+const router = express.Router();
+
+const todos = [];
+let id = 1;
 
 router.get('/todos', function (req, res) {
-    res.send(todos)
+    res.send(todos);
 })
 
 router.post('/todo', function (req, res) {
-    const text = req.body.text
-    const newTodo = { id: id++, text: text, complete: false }
+    const text = req.body.text;
+    const newTodo = { id: id++, text: text, complete: false };
 
-    todos.push(newTodo)
-    res.send(todos)
+    todos.push(newTodo);
+    res.send(todos);
 })
 
 router.put('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID
+    const todoID = req.params.todoID;
 
-    todos.find(t => t.id == todoID).completed = true
-    res.send(todos)
+    todos.find(t => t.id == todoID).completed = true;
+    res.send(todos);
 })
 
 router.delete('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID
-    todos.splice(todoID, 1)
-    console.log('hello')
-    res.send(todos)
+    const todoID = req.params.todoID;
+    todos.splice(todoID, 1);
+    console.log('hello');
+    res.send(todos);
 })
 
-module.exports = router
+module.exports = router;
