526597599888c8d7c2e0f7ac03a0161f73e99a0f
mohmdSalah <mo.s.obidat@gmail.com>
2022-05-15 12:04:43 +0300

    making put & delete routes working

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,35 +1,36 @@
 const express = require('express');
-
-
 const router = express.Router();
 
 const todos = [];
 let id = 1;
 
 router.get('/todos', function (req, res) {
-    res.send(todos);
-})
+  res.send(todos);
+});
 
 router.post('/todo', function (req, res) {
-    const text = req.body.text;
-    const newTodo = { id: id++, text: text, complete: false };
+  const text = req.body.text;
+  const newTodo = { id: id++, text: text, completed: false };
 
-    todos.push(newTodo);
-    res.send(todos);
-})
+  todos.push(newTodo);
+  res.send(todos);
+});
 
 router.put('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID;
-
-    todos.find(t => t.id == todoID).completed = true;
-    res.send(todos);
-})
+  const todoID = req.params.todoID;
+  let todoIdParsed = parseInt(todoID);
+  todos.find((t) => t.id === todoIdParsed).completed = true;
+  res.send(todos);
+});
 
 router.delete('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID;
-    todos.splice(todoID, 1);
-    console.log('hello');
-    res.send(todos);
-})
+  const todoID = req.params.todoID;
+  let todoIdParsed = parseInt(todoID);
+  let todoList = todos.find((t) => t.id === todoIdParsed);
+
+  let index = todos.indexOf(todoList);
+  todos.splice(index, 1);
+  res.send(todos);
+});
 
 module.exports = router;
